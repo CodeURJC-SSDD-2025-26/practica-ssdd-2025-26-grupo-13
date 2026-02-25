79e3a8e9626e360fd41d4cd275bf0dfda8254bab
lucasmp31 <l.morenop.2023@alumnos.urjc.es>
2026-02-25 15:22:05 +0100

    Create review controller and view.

--- webapp/src/main/java/es/mqm/webapp/Product.java
+++ webapp/src/main/java/es/mqm/webapp/Product.java
@@ -1,5 +1,7 @@
 package es.mqm.webapp;
 
+import java.util.ArrayList;
+
 public class Product {
     private String name;
     private String description;
@@ -28,6 +30,4 @@ public class Product {
     public ArrayList<String> getCategories() {
         return categories;
     }
-
-
 }
