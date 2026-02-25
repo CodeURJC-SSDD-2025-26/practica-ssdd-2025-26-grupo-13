495470fb5c1d093ee8f309737201d085ee5c7d98
lucasmp31 <l.morenop.2023@alumnos.urjc.es>
2026-02-25 13:29:37 +0100

    create product class

---
+++ webapp/src/main/java/es/mqm/webapp/Product.java
@@ -0,0 +1,33 @@
+package es.mqm.webapp;
+
+public class Product {
+    private String name;
+    private String description;
+    private double price;
+    private String imageUrl;
+    private  ArrayList<String> categories;
+    public Product(String name, String description, double price, String imageUrl, ArrayList<String> categories) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.categories = categories;
+    }
+    public String getName() {
+        return name;
+    }  
+    public String getDescription() {
+        return description;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public ArrayList<String> getCategories() {
+        return categories;
+    }
+
+
+}
