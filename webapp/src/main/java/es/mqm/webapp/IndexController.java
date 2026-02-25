3cbf28476a12ae3a19bf2f0a754b82e1d316ac5b
diego <diego@rdgz.dev>
2026-02-25 13:33:16 +0100

    add index controller. create static folder for css files.

---
+++ webapp/src/main/java/es/mqm/webapp/IndexController.java
@@ -0,0 +1,15 @@
+package es.mqm.webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+ @GetMapping("/index")
+ public String index(Model model) {
+    model.addAttribute("cssfile", "index");
+    model.addAttribute("loggedin", true);
+ return "index";
+ }
+}
