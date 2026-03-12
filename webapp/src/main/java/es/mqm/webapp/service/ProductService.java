0736cf6b8f33d6ecf80d41f408bb365ddcd75f91
diego <diego@rdgz.dev>
2026-03-12 10:41:03 +0100

    Introduce product pagination and refactor product data access to a dedicated service layer with updated search UI.

--- webapp/src/main/java/es/mqm/webapp/service/ProductService.java
+++ webapp/src/main/java/es/mqm/webapp/service/ProductService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import es.mqm.webapp.model.Product;
@@ -13,7 +16,7 @@ import es.mqm.webapp.repository.ProductRepository;
 public class ProductService {
 
     @Autowired
-	private ProductRepository repository;
+    private ProductRepository repository;
 
     public List<Product> findAll() {
         return repository.findAll();
@@ -21,6 +24,33 @@ public class ProductService {
 
     public Optional<Product> findById(int id) {
         return repository.findById(id);
+    }
+
+    public List<Product> findByName(String name) {
+        return repository.findByNameIgnoreCase(name);
+    }
+
+    public List<Product> findByCategory(String category) {
+        return repository.findByCategoriesContaining(category);
+    }
+
+    public List<Product> findBySeller(String seller) {
+        return repository.findBySeller(seller);
+    }
+
+    public Page<Product> getProducts(int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        return repository.findAll(pageable);
+    }
+
+    public Page<Product> findByName(String name, int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        return repository.findByNameIgnoreCase(name, pageable);
+    }
+
+    public Page<Product> findByCategory(String category, int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        return repository.findByCategoriesContaining(category, pageable);
     }
 
     public Product save(Product product) {
